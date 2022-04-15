f278fdc4f7eaf7b224f720226b1177d023920767
ciceroff <cicero.barrozo13@gmail.com>
2022-04-14 21:19:58 -0300

    refactor: change store method return

--- src/Controller/ForgotPasswordController.js
+++ src/Controller/ForgotPasswordController.js
@@ -8,8 +8,9 @@ module.exports = {
   async store(req, res) {
     try {
       const { email } = req.body
+      const emailRes = String(email)
       const mail = new Mailer()
-      const user = await User.findOne({ where: { email: email } })
+      const user = await User.findOne({ where: { email: emailRes } })
 
       user.token = String(crypto.randomBytes(10).toString('hex'))
       user.token_created_at = new Date()
@@ -21,7 +22,9 @@ module.exports = {
       await user.save()
 
       await mail.resetPassword({ token: user.token, userEmail: user.email })
-      return res.status(200).send({ message: 'E-mail enviado' })
+      return res
+        .status(200)
+        .send({ message: 'E-mail enviado', token: user.token })
     } catch (err) {
       return res.status(500).send({
         error: {
